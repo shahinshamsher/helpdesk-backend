52bd061f6a9a86a110957aeb72bb9816e0f7b100
shahin shamsher <shahinshamsher8@gmail.com>
2025-11-24 01:55:30 +0530

    logs

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -36,12 +36,11 @@ exports.login = async (req, res) => {
 
     const user = await User.findOne({ email });
     if (!user)
-      return res.status(400).json({ message: "Invalid credentials" });
+      return res.status(400).json({ message: "User not found" });
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch)
-      return res.status(400).json({ message: "Invalid credentials" });
-
+      return res.status(400).json({ message: "Invalid password" });
     const token = jwt.sign(
       { id: user._id, role: user.role },
       process.env.JWT_SECRET,
